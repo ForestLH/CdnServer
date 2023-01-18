2adc8865d183a302b7858d939679e2be8473b3bb
Hao Lee <1838249551@qq.com>
2023-01-18 11:46:55 +0800

    test: test github action workflow

--- main.go
+++ main.go
@@ -44,8 +44,6 @@ func main() {
 	group.GET("/ping", func(c context.Context, ctx *app.RequestContext) {
 		ctx.JSON(consts.StatusOK, utils.H{
 			"message": "pong",
-			"from":    "aliyun",
-			"test":    "ci/cd",
 			"author":  "lee",
 		})
 	})
